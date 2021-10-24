076797a62022fabb5dc617b35754ebba62b353fa
Kazukafka <pasuwaado135@gmail.com>
2021-10-24 12:41:51 +0900

    UI2

--- components/ContactsMenu.js
+++ components/ContactsMenu.js
@@ -1,12 +1,81 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+const contactsMenuButtons = [
+  {
+    type: "starred",
+    name: "starred"
+  },
+  {
+    type: "contact",
+    name: "Rimuru Tempest",
+    photo: require("../assets/rimuru.jpeg")
+  },
+  {
+    type: "contact",
+    name: "Hinata Sakaguchi",
+    photo: require("../assets/hinata.jpeg")
+  },
+  {
+    type: "contact",
+    name: "Milim Narva",
+    photo: require("../assets/milim.png")
+  }
+
+]
 
 function ContactsMenu() {
   return (
-    <View>
-      <Text>Conacts Menu</Text>
+    <View style={styles.container}>
+      {/* ContactContainer */}
+      {contactsMenuButtons.map((contact, index) =>
+        <View
+          key={index}
+          style={styles.row}>
+          {/* Image */}
+          {contact.type == "starred" ? (
+            <View style={styles.starredIcon}>
+              <AntDesign name="star" size={30} color="#efefef" />
+            </View>) :
+            (
+              <Image source={contact.photo} style={styles.image} />
+            )
+          }
+          {/* Text */}
+          <Text style={styles.text}>
+            {contact.name}
+          </Text>
+        </View>
+      )}
     </View>
   )
 }
 
 export default ContactsMenu
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    marginTop: 20,
+    alignItems: "center"
+  },
+  starredIcon: {
+    backgroundColor: "#333333",
+    width: 55,
+    height: 55,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 20
+  },
+  text: {
+    color: "white",
+    paddingLeft: 15,
+    fontSize: 18
+  },
+  image: {
+    width: 55,
+    height: 55,
+    borderRadius: 20
+  }
+})
--- components/MenuButton.js
+++ components/MenuButton.js
@@ -1,17 +1,50 @@
-import React from 'react'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
-import FontAwesome from 'react-native-vector-icons/FontAwesome'
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+const items = [
+  {
+    id: 1,
+    name: "video-camera",
+    title: "New Meeting",
+    customColor: "#FF751F",
+  },
+  {
+    id: 2,
+    name: "plus-square",
+    title: "Join",
+  },
+  {
+    id: 3,
+    name: "calendar",
+    title: "Schedule",
+  },
+  {
+    id: 4,
+    name: "upload",
+    title: "Share Screen",
+  }
+]
 
 function MenuButton() {
   return (
     <View style={styles.container}>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button}>
-          <FontAwesome name={"video-camera"} size={23} color={"#efefef"} />
-        </TouchableOpacity>
-        <Text style={styles.menuText}>New Meeting</Text>
-      </View>
-    </View>
+      {/* OneButton */}
+      {items.map((item, index) =>
+        <View
+          key={index}
+          style={styles.buttonContainer}>
+          <TouchableOpacity style={{
+            ...styles.button,
+            backgroundColor: item.customColor ? item.customColor : "#0470DC"
+          }}>
+            <FontAwesome name={item.name} size={23} color={"#efefef"} />
+          </TouchableOpacity>
+          <Text style={styles.menuText}>{item.title}</Text>
+        </View>
+      )
+      }
+    </View >
   )
 }
 
@@ -22,22 +55,24 @@ const styles = StyleSheet.create({
     marginTop: 25,
     paddingBottom: 10,
     borderBottomColor: "#1F1F1F",
-    borderBottomWidth: 1
+    borderBottomWidth: 1,
+    flexDirection: "row",
+    justifyContent: "space-between"
   },
   buttonContainer: {
-    alignItems: "center"
+    alignItems: "center",
+    flex: 1
   },
   button: {
     width: 50,
     height: 50,
-    backgroundColor: "blue",
     borderRadius: 15,
     justifyContent: "center",
     alignItems: "center"
   },
   menuText: {
     color: "#858585",
-    fontSize: 20,
+    fontSize: 12,
     paddingTop: 10,
     fontWeight: "600"
   }
